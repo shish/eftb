5ca64714c7ddf75823265d792c754310a7030e61
Shish <shish@shishnet.org>
2025-04-11 18:56:54 +0100

    update ships for c3

--- src/consts.tsx
+++ src/consts.tsx
@@ -7,17 +7,15 @@ export type Ships = {
   [key: string]: Ship;
 };
 export const ships: Ships = {
-  Explorer: { mass: 5000000, fuel: 280, fuel_type: "uSOF-30" },
-  Forager: { mass: 11000000, fuel: 63, fuel_type: "uSOF-30" },
-  Juav: { mass: 14000000, fuel: 182, fuel_type: "uSOF-30" },
-  Microptero: { mass: 17000000, fuel: 245, fuel_type: "SOF-40" },
-  Val: { mass: 28000000, fuel: 539, fuel_type: "SOF-40" },
-  Flegel: { mass: 145000000, fuel: 2990, fuel_type: "SOF-40" },
-  Anser: { mass: 285000000, fuel: 7090, fuel_type: "SOF-40" },
-  Dremar: { mass: 70250000, fuel: 1100, fuel_type: "SOF-40" },
-  Axte: { mass: 807000000, fuel: 22500, fuel_type: "SOF-40" },
-  Caruda: { mass: 1400000000, fuel: 38900, fuel_type: "SOF-40" },
-  Grus: { mass: 2425000000, fuel: 66200, fuel_type: "SOF-40" },
+  // Confirmed in cycle 3
+  Explorer: { mass: 4_500_000, fuel: 230, fuel_type: "uSOF-20" },
+  Forager: { mass: 8_000_000, fuel: 120, fuel_type: "uSOF-20" },
+  Juav: { mass: 14_000_000, fuel: 360, fuel_type: "uSOF-20" },
+  // Unconfirmed
+  Microptero: { mass: 17_000_000, fuel: 245, fuel_type: "SOF-40" },
+  Val: { mass: 28_000_000, fuel: 539, fuel_type: "SOF-40" },
+  Flegel: { mass: 145_000_000, fuel: 2990, fuel_type: "SOF-40" },
+  Anser: { mass: 285_000_000, fuel: 7090, fuel_type: "SOF-40" },
 } as const;
 
 export const fuels = {
