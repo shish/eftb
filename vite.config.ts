74e7fd45759f69edb97c04d2726be8f78fc303ff
Shish <shish@shishnet.org>
2026-03-27 15:35:01 +0000

    add build:analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter(), react()],
+  plugins: [
+    tanstackRouter(),
+    react(),
+    ...(process.env.ANALYZE
+      ? [
+          visualizer({
+            filename: "dist/stats.html",
+            gzipSize: true,
+            brotliSize: true,
+          }),
+        ]
+      : []),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom")
+          ) {
+            return "react";
+          }
+          if (id.includes("node_modules/@tanstack")) {
+            return "tanstack";
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
